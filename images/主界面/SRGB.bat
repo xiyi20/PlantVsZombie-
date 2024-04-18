fbc6dfaefcba36d69f2ed4258c3d5689769ff1d1
Mr.Wood <89333768+xiyi20@users.noreply.github.com>
2024-04-18 18:48:13 +0800

    Delete images/主界面/SRGB.bat
    去除一个不必要文件

--- images/主界面/SRGB.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo ImageMagick fix libpng warning: iCCP: Not recognizing known sRGB profile ......
-echo Search PNG in subdirs and process ...
-set fn=F:\ImageMagick\convert.exe
-for /f "tokens=*" %%i in ('dir/s/b *.png') do "%fn%" "%%i" -strip "%%i"
-pause
