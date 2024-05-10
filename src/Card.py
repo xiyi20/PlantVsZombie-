5977d7d5f84c604653207b280906f073adb57156
Mr.Wood <3229429589@qq.com>
2024-05-10 11:03:56 +0800

    🎈 perf: 优化卡片信息显示

--- src/Card.py
+++ src/Card.py
@@ -1,6 +1,6 @@
 import pygame
 import Const
-from Source import objectType, getImageSource, priceText, screen, menuText
+from Source import objectType, getImageSource, priceText, screen
 
 
 class Card:
@@ -11,8 +11,8 @@ class Card:
         self.selected = None
         self.active = False
         self.price = str(price)
-        self.coolimage = objectType[11]
-        self.cooltime = str(cooltime)
+        self.coolImage = objectType[11]
+        self.coolTime = str(cooltime)
         self.interval = Const.CARD_HEIGHT / cooltime
         self.curTime = '0'
         self.game = gameObj
@@ -22,19 +22,19 @@ class Card:
     def draw(self):
         self.rect = pygame.Rect(
             self.x, self.y, Const.CARD_WIDTH, Const.CARD_HEIGHT)
-        plantcard = pygame.transform.scale(getImageSource(
+        plantCard = pygame.transform.scale(getImageSource(
             'img/widget/卡槽.png'), (Const.CARD_WIDTH, Const.CARD_HEIGHT))
-        plantimage = pygame.transform.scale(getImageSource(
+        plantImage = pygame.transform.scale(getImageSource(
             self.img), (Const.CARD_IMG_SIZE, Const.CARD_IMG_SIZE))
-        screen.blit(plantcard, (self.x, self.y))
-        screen.blit(plantimage, (self.x + 4, self.y + 12))
+        screen.blit(plantCard, (self.x, self.y))
+        screen.blit(plantImage, (self.x + 4, self.y + 12))
         color = (255, 0, 0) if int(
-            self.price) > self.game.playSun and not self.game.selectFlag else (0, 0, 0)
+            self.price) > self.game.curSun and not self.game.selectFlag else (0, 0, 0)
         screen.blit(priceText.render(self.price, True, color),
                     (self.x + 2, self.y + 55))
         if self.selected:
             screen.blit(self.selected, (self.x, self.y + 1))
         if self.curTime != '0':
-            screen.blit(pygame.transform.scale(self.coolimage, (50, int(self.curTime) * self.interval)),
+            screen.blit(pygame.transform.scale(self.coolImage, (50, int(self.curTime) * self.interval)),
                         (self.x, self.y))
             self.curTime = str(int(self.curTime) - 1)
